223ea170b070514e5527081c7f580367461ab584
PWN0N <juangshin@gmail.com>
2018-04-05 13:58:35 +0800

    reconstruct the QRcode Detector

--- main.cpp
+++ main.cpp
@@ -1,202 +1,360 @@
-
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <Eigen/Core>
-#include <Eigen/Dense>
+#include <string>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <chrono>
 
-using namespace cv;
 using namespace std;
-using namespace Eigen;
-//找到所提取轮廓的中心点
-//在提取的中心小正方形的边界上每隔周长个像素提取一个点的坐标，求所提取四个点的平均坐标（即为小正方形的大致中心）
-Point Center_cal(vector<vector<Point> > contours,int i);
-void getQRCodeCorners(Mat src,vector<Point>& QRCodeCorners);
+using namespace cv;
 
-int main( int argc, char** argv[] )
+RNG rng(12345);
+
+struct Index
 {
-    Mat src = imread( "/home/jonathan/QRCode/7.jpg", 1 );
-    resize(src,src,Size(800,600));//标准大小
-    vector<Point> QRcorner;
-
-    getQRCodeCorners(src,QRcorner);
-
+    int a1;
+    int a2;
+    int a3;
+};
 
 
-    line(src,QRcorner[0],QRcorner [1],Scalar(0,0,255),2);
-    line(src,QRcorner [1],QRcorner [2],Scalar(0,255,0),2);
-    line(src,QRcorner [2],QRcorner[3],Scalar(255,0,0),2);
-    line(src,QRcorner [3],QRcorner[0],Scalar(255,0,255),2);
-    circle(src,QRcorner[0],5,Scalar(0,0,255),3,8,0);
-    circle(src,QRcorner[1],5,Scalar(0,0,255),3,8,0);
-    circle(src,QRcorner[2],5,Scalar(0,0,255),3,8,0);
-    circle(src,QRcorner[3],5,Scalar(0,0,255),3,8,0);
-    imshow("sed",src);
-    waitKey(0);
-
-    Matrix<float,2,3> pr;
-    pr << 1,0,0,
-            0,1,0 ;
-    Matrix<float,3,3> K;
-    K << 3266.96772,0,2013.15663,
-            0,3267.10869,1482.03236,
-            0,0,1;
-    Matrix<float,3,4> Qici ;
-    Qici << QRcorner[0].x,QRcorner[1].x,QRcorner[2].x,QRcorner[3].x,
-            QRcorner[0].y,QRcorner[1].y,QRcorner[2].y,QRcorner[3].y,
-            1,1,1,1;
-    Matrix<float,3,4> CameraPoint = K.inverse()*Qici;
-
-    Matrix<float,4,2> p;
-    Matrix<float,2,4> ptemp;
-    ptemp = (pr*CameraPoint);
-//    p = ptemp.transpose();
-    p = ptemp.transpose();
-
-    cout << p << endl;
-
-    return 0;
 
 
-//    Mat p = (pr*());K\qici
-//                         H = est_homography(corner_pts,p);
-//                         [proj_pts, pos{i}, rot{i}] = ar_cube(H,render_points,K);
-//    % Draw a coordinate frame
-//    frame = 0.05 * [0 0 0; 1 0 0; 0 1 0; 0 0 1];
-//    frame = bsxfun(@plus,frame * rot{i}',pos{i}') * K';
-//    frame(:, 1) = frame(:, 1) ./ frame(:, 3);
-//    frame(:, 2) = frame(:, 2) ./ frame(:, 3);
-//    frame = frame(:, 1:2);
-//    generated_imgs{i} = insertShape(generated_imgs{i}, ...
-//    'Line',[frame(1,:) frame(2,:)], ...
-//    'Color','red','LineWidth',4);
-//    generated_imgs{i} = insertShape(generated_imgs{i}, ...
-//    'Line',[frame(1,:) frame(3,:)], ...
-//    'Color','green','LineWidth',4);
-//    generated_imgs{i} = insertShape(generated_imgs{i}, ...
-//    'Line',[frame(1,:) frame(4,:)], ...
-//    'Color','blue','LineWidth',4);
-//
-//    % Copy the RGB values from the logo_img to the video frame
-//    generated_imgs{i} = draw_ar_cube(proj_pts,generated_imgs{i});
-}
+//获取轮廓的中心点
 Point Center_cal(vector<vector<Point> > contours,int i)
 {
     int centerx=0,centery=0,n=contours[i].size();
+    //在提取的小正方形的边界上每隔周长个像素提取一个点的坐标，
+    //求所提取四个点的平均坐标（即为小正方形的大致中心）
     centerx = (contours[i][n/4].x + contours[i][n*2/4].x + contours[i][3*n/4].x + contours[i][n-1].x)/4;
     centery = (contours[i][n/4].y + contours[i][n*2/4].y + contours[i][3*n/4].y + contours[i][n-1].y)/4;
     Point point1=Point(centerx,centery);
     return point1;
 }
-void getQRCodeCorners(Mat src,vector<Point>& QRCodeCorners){
+#define onepic 1
+//#define DEBUG
+Index in;
+vector<Index> vin;
+int main( int argc, char** argv[] )
+{
 
-    Mat src_gray;
-    Mat src_all=src.clone();
-    Mat threshold_output;
-    vector<vector<Point> > contours,contours2;
-    vector<Vec4i> hierarchy;
-    //预处理
-    cvtColor( src, src_gray, CV_BGR2GRAY );
-    blur( src_gray, src_gray, Size(3,3) ); //模糊，去除毛刺
-    threshold( src_gray, threshold_output, 100, 255, THRESH_OTSU );
-    //寻找轮廓
-    //第一个参数是输入图像 2值化的
-    //第二个参数是内存存储器，FindContours找到的轮廓放到内存里面。
-    //第三个参数是层级，**[Next, Previous, First_Child, Parent]** 的vector
-    //第四个参数是类型，采用树结构
-    //第五个参数是节点拟合模式，这里是全部寻找
-    findContours( threshold_output, contours, hierarchy,  CV_RETR_TREE, CHAIN_APPROX_NONE, Point(0, 0) );
-    //轮廓筛选
-    int c=0,ic=0,area=0;
-    int parentIdx=-1;
-    for( int i = 0; i< contours.size(); i++ )
+    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+    string path[20];
+    int k = 0;
+    for(int i = 1;i<=5;i++)
     {
-        //hierarchy[i][2] != -1 表示不是最外面的轮廓
-        if (hierarchy[i][2] != -1 && ic==0)
+        for(int j = 7;j<=10;j++)
         {
-            parentIdx = i;
-            ic++;
-        }
-        else if (hierarchy[i][2] != -1)
-        {
-            ic++;
-        }
-            //最外面的清0
-        else if(hierarchy[i][2] == -1)
-        {
-            ic = 0;
-            parentIdx = -1;
-        }
-        //找到定位点信息
-        if ( ic >= 2)
-        {
-            contours2.push_back(contours[parentIdx]);
-            ic = 0;
-            parentIdx = -1;
+            string s1 = "./qrcodepic/";
+
+            string s2 ;
+            stringstream ss;
+            ss << i;
+            s2 = ss.str();
+
+            stringstream ss2;
+            string s3;
+            ss2 << j%10;
+            s3 = ss2.str();
+
+            string s4 = "0.png";
+
+            path[k++] = s1+s2+s3+s4;
         }
     }
-    //填充定位点
-    for(int i=0; i<contours2.size(); i++)
-
-        drawContours( src_all, contours2, i,  CV_RGB(0,255,0) , 2 );
-
-    //连接定位点
-    Point point[3];
-
-    for(int i=0; i<contours2.size(); i++)
+#if onepic == 1
+    k = 1;
+#endif
+    for(int kk = 0;kk<k;kk++)
     {
-        point[i] = Center_cal( contours2, i );
-        QRCodeCorners.push_back(point[i]);
+#if onepic == 1
+        //Mat src = imread( "qrcodepic\\590.png", 1 );
+        Mat src = imread( "./pic/456.jpg", 1 );
+
+
+        if(src.empty())
+        {
+            cout << "Can not load image！" << endl;
+            return 0;
+        }
+
+        Mat src_all=src.clone();
+
+        //彩色图转灰度图
+        Mat src_gray;
+        cvtColor( src, src_gray, CV_BGR2GRAY );
+
+        //对图像进行平滑处理
+        //blur( src_gray, src_gray, Size(3,3) );
+
+        //使灰度图象直方图均衡化
+        //equalizeHist( src_gray, src_gray );
+
+#ifdef DEBUG
+        namedWindow("src_gray");
+        imshow("src_gray",src_gray);         //灰度图
+#endif
+
+        //指定112阀值进行二值化
+        Mat threshold_output;
+        threshold( src_gray, threshold_output, 112, 255, THRESH_BINARY );
+
+#ifdef DEBUG
+        //namedWindow("二值化后输出");
+        //imshow("二值化后输出",threshold_output);   //二值化后输出
+#endif
+
+        //需要的变量定义
+        Scalar color = Scalar(1,1,255 );
+        vector<vector<Point>> contours,contours2;
+        vector<Vec4i> hierarchy;
+        Mat drawing = Mat::zeros( src.size(), CV_8UC3 );
+        //Mat drawing2 = Mat::zeros( src.size(), CV_8UC3 );
+        Mat drawingAllContours = Mat::zeros( src.size(), CV_8UC3 );
+
+        //利用二值化输出寻找轮廓
+        findContours(threshold_output, contours, hierarchy,  CV_RETR_TREE, CHAIN_APPROX_NONE, Point(0, 0) );
+
+        //寻找轮廓的方法
+        int tempindex1 = 0;
+        int tempindex2 = 0;
+
+        for(int i = 0;i<contours.size();i++)
+        {
+            if(hierarchy[i][2] == -1)
+                continue;
+            else
+                tempindex1 = hierarchy[i][2];                //第一个子轮廓的索引
+
+            if(hierarchy[tempindex1][2] == -1)
+                continue;
+            else
+            {
+                tempindex2 = hierarchy[tempindex1][2];        //第二个子轮廓的索引
+                //记录搜索到的有两个子轮廓的轮廓并把他们的编号存储
+                in.a1 = i;
+                in.a2 = tempindex1;
+                in.a3 = tempindex2;
+                vin.push_back(in);
+            }
+        }
+
+        //按面积比例搜索
+        vector<Index>::iterator it;
+        for(it = vin.begin();it != vin.end();)
+        {
+            vector<Point> out1Contours = contours[it->a1];
+            vector<Point> out2Contours = contours[it->a2];
+            double lenth1 = arcLength(out1Contours,1);
+            double lenth2 = arcLength(out2Contours,1);
+            if(abs(lenth1/lenth2-2)>1)
+            {
+                it = vin.erase(it);
+            }
+            else
+            {
+                drawContours( drawing, contours, it->a1,  CV_RGB(255,255,255) , CV_FILLED, 8); //rng.uniform(0,255)
+                it++;
+            }
+        }
+
+        //填充的方式画出三个黑色定位角的轮廓
+        //for(int i=0; i<contours2.size(); i++)
+        //	drawContours( drawing2, contours2, i,  CV_RGB(rng.uniform(100,255),rng.uniform(100,255),rng.uniform(100,255)) , -1, 4, hierarchy[k][2], 0, Point() );
+
+        //获取三个定位角的中心坐标
+        Point point[3];
+        int i = 0;
+        vector<Point> pointthree;
+        for(it = vin.begin(),i = 0;it != vin.end();i++,it++)
+        {
+            point[i] = Center_cal( contours, it->a1 );
+            pointthree.push_back(point[i]);
+        }
+
+        if(pointthree.size() <3)
+        {
+            cout << "找到的定位角点不足3个"<<endl;
+            return 0;
+        }
+
+        //计算轮廓的面积，计算定位角的面积，从而计算出边长
+        double area = contourArea(contours[vin[0].a1]);
+        int area_side = cvRound (sqrt (double(area)));
+        for(int i=0; i<3; i++)
+        {
+            //画出三个定位角的中心连线
+            line(drawing,point[i%3],point[(i+1)%3],color,area_side/10,8);
+        }
+
+        //清除找到的3个点,以便处理下一幅图片使用
+        vin.clear();
+
+        //由3个定位角校正图片
+        //=========================================
+        //找到角度最大的点
+        double ca[2];
+        double cb[2];
+
+        ca[0] =  pointthree[1].x - pointthree[0].x;
+        ca[1] =  pointthree[1].y - pointthree[0].y;
+        cb[0] =  pointthree[2].x - pointthree[0].x;
+        cb[1] =  pointthree[2].y - pointthree[0].y;
+        double angle1 = 180/3.1415*acos((ca[0]*cb[0]+ca[1]*cb[1])/(sqrt(ca[0]*ca[0]+ca[1]*ca[1])*sqrt(cb[0]*cb[0]+cb[1]*cb[1])));
+        double ccw1;
+        if(ca[0]*cb[1] - ca[1]*cb[0] > 0) ccw1 = 0;
+        else ccw1 = 1;
+
+        ca[0] =  pointthree[0].x - pointthree[1].x;
+        ca[1] =  pointthree[0].y - pointthree[1].y;
+        cb[0] =  pointthree[2].x - pointthree[1].x;
+        cb[1] =  pointthree[2].y - pointthree[1].y;
+        double angle2 = 180/3.1415*acos((ca[0]*cb[0]+ca[1]*cb[1])/(sqrt(ca[0]*ca[0]+ca[1]*ca[1])*sqrt(cb[0]*cb[0]+cb[1]*cb[1])));
+        double ccw2;
+        if(ca[0]*cb[1] - ca[1]*cb[0] > 0) ccw2 = 0;
+        else ccw2 = 1;
+
+        ca[0] =  pointthree[1].x - pointthree[2].x;
+        ca[1] =  pointthree[1].y - pointthree[2].y;
+        cb[0] =  pointthree[0].x - pointthree[2].x;
+        cb[1] =  pointthree[0].y - pointthree[2].y;
+        double angle3 = 180/3.1415*acos((ca[0]*cb[0]+ca[1]*cb[1])/(sqrt(ca[0]*ca[0]+ca[1]*ca[1])*sqrt(cb[0]*cb[0]+cb[1]*cb[1])));
+        double ccw3;
+        if(ca[0]*cb[1] - ca[1]*cb[0] > 0) ccw3 = 0;
+        else ccw3 = 1;
+
+        CvPoint2D32f poly[4];
+        if(angle3>angle2 && angle3>angle1)
+        {
+            if(ccw3)
+            {
+                poly[1] = pointthree[1];
+                poly[3] = pointthree[0];
+            }
+            else
+            {
+                poly[1] = pointthree[0];
+                poly[3] = pointthree[1];
+            }
+            poly[0] = pointthree[2];
+        }
+        else if(angle2>angle1 && angle2>angle3)
+        {
+            if(ccw2)
+            {
+                poly[1] = pointthree[0];
+                poly[3] = pointthree[2];
+            }
+            else
+            {
+                poly[1] = pointthree[2];
+                poly[3] = pointthree[0];
+            }
+            poly[0] = pointthree[1];
+
+        }
+        else if(angle1>angle2 && angle1 > angle3)
+        {
+            if(ccw1)
+            {
+                poly[1] = pointthree[1];
+                poly[3] = pointthree[2];
+            }
+            else
+            {
+                poly[1] = pointthree[2];
+                poly[3] = pointthree[1];
+            }
+            poly[0] = pointthree[0];
+        }
+
+        Point2f polyThrPts[3];
+        polyThrPts[0]=poly[3];
+        polyThrPts[1]=poly[0];
+        polyThrPts[2]=poly[1];
+
+        CvPoint2D32f trans[4];
+        int temp = 50;
+        trans[0] = Point2f(0+temp,0+temp);
+        trans[1] = Point2f(0+temp,100+temp);
+        trans[2] = Point2f(100+temp,100+temp);
+        trans[3] = Point2f(100+temp,0+temp);
+
+        Point2f transThrPts[3];
+        transThrPts[0]=trans[3];
+        transThrPts[1]=trans[0];
+        transThrPts[2]=trans[1];
+
+        //获取透视投影变换矩阵
+
+        Mat transMat = getAffineTransform(polyThrPts, transThrPts);
+
+        Mat transback = getAffineTransform(transThrPts , polyThrPts);
+
+
+        poly[2].x = (int)(transback.at<double>(0, 0) * (double)trans[2].x
+                          + transback.at<double>(0, 1) * (double)trans[2].y
+                          + transback.at<double>(0, 2));
+        poly[2].y = (int)(transback.at<double>(1, 0) * (double)trans[2].x
+                          + transback.at<double>(1, 1) * (double)trans[2].y
+                          + transback.at<double>(1, 2));
+
+
+        //计算变换结果
+
+        Mat src_img(src_all);
+        Mat transedImg = Mat::zeros(src_img.rows, src_img.cols, src_img.type());
+        warpAffine(src_img, transedImg, transMat, transedImg.size());
+
+        //=========================================
+
+#ifdef DEBUG
+        namedWindow("透视变换后的图");
+        imshow("透视变换后的图",transedImg);         //透视变换后的图
+
+        drawContours( drawingAllContours, contours, -1,  CV_RGB(255,255,255) , 1, 8);
+        namedWindow("DrawingAllContours");
+        imshow( "DrawingAllContours", drawingAllContours );
+
+        namedWindow(pathtemp);
+        imshow(pathtemp , drawing );    //3个角点填充
+
+#endif
+
+        //接下来要框出这整个二维码
+        Mat gray_all,threshold_output_all;
+        vector<vector<Point> > contours_all;
+        vector<Vec4i> hierarchy_all;
+        cvtColor( drawing, gray_all, CV_BGR2GRAY );
+
+        threshold( gray_all, threshold_output_all, 45, 255, THRESH_BINARY );
+
+        findContours( threshold_output_all, contours_all, hierarchy_all,  RETR_EXTERNAL, CHAIN_APPROX_NONE, Point(0, 0) );//RETR_EXTERNAL表示只寻找最外层轮廓
+
+        Point2f fourPoint2f[4];
+        //求最小包围矩形
+        RotatedRect rectPoint = minAreaRect(contours_all[0]);
+
+        //将rectPoint变量中存储的坐标值放到 fourPoint的数组中
+        rectPoint.points(fourPoint2f);
+        for (int i = 0; i < 4; i++)
+        {
+            line(src_all, fourPoint2f[i%4], fourPoint2f[(i + 1)%4],
+                 Scalar(20,21,237), 3);
+            circle(src_all, poly[i%4], 5, Scalar(0, 255, 0), 5, 8, 0);
+        }
+//        circle(src_all, poly[2], 5, Scalar(0, 255, 0), 5, 8, 0);
+#ifdef DEBUG
+        namedWindow(pathtemp);
+        imshow(pathtemp , src_all );
+#endif
+        std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+        double ttrack= std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
+        std::cout<< "frame_times = "<<ttrack<<"s"<<std::endl;
+        waitKey(0);
+
     }
-
-}
-
-Matrix<float,3,3> getHomoMatrix(Matrix<float,4,2>  cube_pts,Matrix<float,4,2> world_pts){
-
-    Matrix<float,8,9>  A ;
-    Matrix<float,1,9> ax ;
-    Matrix<float,1,9> ay ;
-
-    for (int i = 0; i < 3 ; ++i) {
-        ax << (-1)*cube_pts(i,1),(-1)*cube_pts(i,2),-1,0,0,0,cube_pts(i,1)*world_pts(i,1),cube_pts(i,2)*world_pts(i,1),world_pts(i,1);
-        ay << 0,0,0,(-1)*cube_pts(i,1),(-1)*cube_pts(i,2),-1,cube_pts(i,1)*world_pts(i,2),cube_pts(i,2)*world_pts(i,2),world_pts(i,2);
-        int m = 2*i-1;
-        A(m:m+1,:) = [ax;ay];
-
-    }
-
-    JacobiSVD<Eigen::MatrixXf> svd(A, ComputeThinU | ComputeThinV );
-    Matrix3f V = svd.matrixV(), U = svd.matrixU();
-    Matrix3f  S = U.inverse() * A * V.transpose().inverse();
-
-    Matrix<float,3,3> H = [reshape(V(:,end),[3,3])]';
-    return H;
-}
-
-void getARcube(Matrix3f H,Matrix3f render_points,Matrix3f K){
-
-                                                                                                                                                                                                                                                                                             %��Ϊÿ��ͼ��Ķ�ά�붼�����Ϊ��ģ����Ա��뱣֤t���һάΪ��H(3,3)ҪΪ��
-    if H(3,3)<0
-    H = -H;
-
-
-
-            tempH = [H(:,1),H(:,2),cross(H(:,1),H(:,2))];
-
-    JacobiSVD<Eigen::MatrixXf> svd(tempH, ComputeThinU | ComputeThinV );
-    Matrix3f V = svd.matrixV(), U = svd.matrixU();
-    Matrix3f S << 1,0,0,0,1,0,0,0,U*V.inverse();;
-
-    [U,~,V] = svd(tempH);
-
-
-    R = U*S*V';
-    t = H(:,3)/norm(tempH(:,1));
-
-    proj_points_homo = K*(R*render_points'+t);
-
-    proj_points = [proj_points_homo(1,:)./proj_points_homo(3,:);proj_points_homo(2,:)./proj_points_homo(3,:)]';
+    waitKey();
+    return 0;
 }
